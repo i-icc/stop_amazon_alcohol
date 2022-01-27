3f5e24174ef14489ced1cef8555c9c80f23ef06a
新田一斗 <shintaichito@shintaichitonoMacBook-Air.local>
2022-01-27 18:31:32 +0900

    create getAlcol

--- chrome_stop_amazon_alcohol/src/content.js
+++ chrome_stop_amazon_alcohol/src/content.js
@@ -1,44 +1,51 @@
 'use strict';
 
 let flag = true;
+let alcoholScore = 0;
 let port;
+let score;
 
 $("#addToCart_feature_div").before('<div class="a-button-stack"><span class="a-button a-spacing-small a-button-primary a-button-icon buybox-button-enhancement-size"><span class="a-button-inner"><input id="connect-device" class="a-button-input attach-dss-atc" type="button"><span class="a-button-text">センサーに接続する</span></span></span></div>');
 
-function judgeAlcohpl(){
-  port.connect().then(() => {
-    console.log(port);
-    /* port.onReceive = data => {
-      console.log(data)
-    } */
-  }, error => {
-    console.log("接続中エラーが生じました。")
-  });
+async function judgeAlcohpl(){
+  await getAlcohol()
+  console.log(score)
+  if (score > alcoholScore){
+    alcoholScore = score;
+  }
 }
 
-function stopAmazonAlcohol(){
+async function judgeConect(){
+  return serial.getPorts().then(ports => {
+    let is_connect = null;
+    if (ports.length == 0) {
+      alert("センサーに接続してください。")
+      port = undefined;
+      flag = true;
+      is_connect = false;
+    } else {
+      port = ports[0];
+      is_connect = true;
+    }
+    return is_connect
+  })
+}
+
+async function stopAmazonAlcohol(){
     alert("酔っ払っていませんか？\nセンサーに息を吹きかけてください");
     // なんやかんや
 
-    serial.getPorts().then(ports => {
-      if (ports.length == 0) {
-        alert("センサーに接続してください。")
-        port = undefined
-        flag = true
-        return
-      } else {
-        port = ports[0];
-        console.log("conected")
-      }
-    })
-
-    console.log(initialize())
-    // flag = false
-
-    if (flag){
-      alert("あなたはシラフです。")
+    // let is_connect = await judgeConect()
+    if (! await judgeConect()){
+      return
     } else {
-        // 別ページに飛ばす
+      await judgeAlcohpl()
+      flag = alcoholScore < 400;
+      if (! flag){
+        alert("あなたはシラフです。")
+      } else {
+          // 別ページに飛ばす
+      }
     }
 }
 
@@ -57,7 +64,6 @@ $("#connect-device").click(function() {
   } else {
     serial.requestPort().then(selectedPort => {
       port = selectedPort;
-      initialize()
     }).catch(error => {
       alert("接続に失敗しました")
     });
@@ -69,22 +75,20 @@ serial.getPorts().then(ports => {
     console.log("no connection")
   } else {
     port = ports[0];
-    initialize()
     console.log(port)
   }
 })
 
-function initialize() {
-  port.connect().then(() => {
+async function getAlcohol() {
+  return port.connect().then(() => {
     port.onReceive = data => {
       let textDecoder = new TextDecoder();
-      console.log(textDecoder.decode(data))
-      return textDecoder.decode(data);
+      score = Number(textDecoder.decode(data))
+      console.log(score)
+      return score
     }
-    port.onReceiveError = error => {
-      console.log('Receive error: ' + error);
-    };
-  }, error => {
-    console.log('Connection error: ' + error);
+    console.log(port.onReceive())
+    console.log(port)
+    return port.onReceive()
   });
 };
